827f277bf66405a85b810f6e24eb4ce92b48ee8c
Tobe O <thosakwe@gmail.com>
2019-09-25 18:35:23 -0400

    GopherRequest implements IOSink

--- gopher/lib/src/request.dart
+++ gopher/lib/src/request.dart
@@ -1,8 +1,9 @@
 import 'dart:async';
+import 'dart:convert';
 import 'dart:io';
 import 'package:async/async.dart';
 
-class GopherRequest {
+class GopherRequest implements IOSink {
   final String path;
   final Socket socket;
   final StreamQueue<String> lines;
@@ -15,4 +16,39 @@ class GopherRequest {
     lines.rest.drain();
     return Future.value();
   }
+
+  @override
+  Encoding get encoding => socket.encoding;
+
+  @override
+  set encoding(Encoding value) => socket.encoding = value;
+
+  @override
+  void add(List<int> data) => socket.add(data);
+
+  @override
+  void addError(Object error, [StackTrace stackTrace]) =>
+      socket.addError(error, stackTrace);
+
+  @override
+  Future addStream(Stream<List<int>> stream) => socket.addStream(stream);
+
+  @override
+  Future get done => socket.done;
+
+  @override
+  Future flush() => socket.flush();
+
+  @override
+  void write(Object obj) => socket.write(obj);
+
+  @override
+  void writeAll(Iterable objects, [String separator = ""]) =>
+      socket.writeAll(objects, separator);
+
+  @override
+  void writeCharCode(int charCode) => socket.writeCharCode(charCode);
+
+  @override
+  void writeln([Object obj = ""]) => socket.writeln(obj);
 }
