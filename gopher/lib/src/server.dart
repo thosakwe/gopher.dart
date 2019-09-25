d4a5e042f9686c7323764a007eb961564686d7e6
Tobe O <thosakwe@gmail.com>
2019-09-25 18:12:36 -0400

    Create GopherServer class skeleton

---
+++ gopher/lib/src/server.dart
@@ -0,0 +1,43 @@
+import 'dart:async';
+import 'dart:io';
+
+class GopherServer {
+  ServerSocket _serverSocket;
+  SecureServerSocket _secureServerSocket;
+  Stream<Socket> _socketStream;
+
+  GopherServer.listenOn(this._serverSocket) : _socketStream = _serverSocket;
+
+  GopherServer.listenOnSecure(this._secureServerSocket)
+      : _socketStream = _secureServerSocket;
+
+  static Future<GopherServer> bind(address, int port,
+      {int backlog = 0, bool shared = false, bool v6Only = false}) async {
+    var socket = await ServerSocket.bind(address, port,
+        backlog: backlog, shared: shared, v6Only: v6Only);
+    return GopherServer.listenOn(socket);
+  }
+
+  static Future<GopherServer> bindSecure(
+      address, int port, SecurityContext context,
+      {int backlog: 0,
+      bool v6Only: false,
+      bool requestClientCertificate: false,
+      bool requireClientCertificate: false,
+      List<String> supportedProtocols,
+      bool shared: false}) async {
+    var socket = await SecureServerSocket.bind(address, port, context,
+        backlog: backlog,
+        shared: shared,
+        v6Only: v6Only,
+        requestClientCertificate: requestClientCertificate,
+        requireClientCertificate: requireClientCertificate,
+        supportedProtocols: supportedProtocols);
+    return GopherServer.listenOnSecure(socket);
+  }
+
+  Future<void> close() async {
+    await _serverSocket?.close();
+    await _secureServerSocket?.close();
+  }
+}
