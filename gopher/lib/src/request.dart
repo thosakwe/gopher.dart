e4b355b6db100a34a1db68b3816cef97fd465a70
Tobe O <thosakwe@gmail.com>
2019-09-25 18:43:31 -0400

    Add response functions

--- gopher/lib/src/request.dart
+++ gopher/lib/src/request.dart
@@ -1,6 +1,24 @@
 import 'dart:async';
 import 'dart:io';
+import 'package:charcode/ascii.dart';
 import 'package:async/async.dart';
+
+abstract class GopherItemType {
+  static const String file = '0',
+      directory = '1',
+      csoPhoneBookServer = '2',
+      error = '3',
+      binHexedMacintoshFile = '4',
+      dosBinaryArchive = '5',
+      unixUUEncodedFile = '6',
+      indexSearchServer = '7',
+      textBasedTelnetSession = '8',
+      binaryFile = '9',
+      redundantServer = '+',
+      textBasedTN3270Session = 'T',
+      gifFormatGraphicsFile = 'g',
+      imageFile = 'I';
+}
 
 class GopherRequest {
   final String path;
@@ -8,6 +26,20 @@ class GopherRequest {
   final StreamQueue<String> lines;
 
   GopherRequest(this.path, this.socket, this.lines);
+
+  Future<void> writeStream(type, Stream<List<int>> stream) async {
+    socket.write(type);
+    await socket.addStream(stream);
+  }
+
+  void writeCrlf() => socket.add([$cr, $lf]);
+
+  void writeItem(type, [Iterable values]) {
+    var v = values?.join('F');
+    socket.write(type);
+    if (v != null) socket.write(v);
+    writeCrlf();
+  }
 
   Future<void> close() {
     socket.writeln('.');
