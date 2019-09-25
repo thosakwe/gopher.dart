da81e899c14af12020674138bd315d1e8810829f
Tobe O <thosakwe@gmail.com>
2019-09-25 19:19:30 -0400

    Angel gopher example

---
+++ angel_gopher/example/main.dart
@@ -0,0 +1,15 @@
+import 'package:angel_framework/angel_framework.dart';
+import 'package:angel_gopher/angel_gopher.dart';
+import 'package:logging/logging.dart';
+
+main() async {
+  var logger = Logger('example')..onRecord.listen(print);
+  var app = Angel(logger: logger), gopher = AngelGopher(app);
+
+  app.get('/', (req, res) => res.write(''));
+
+  app.get('/json', (req, res) => {'this': 'is json!!!'});
+
+  await gopher.startServer('127.0.0.1', 7070);
+  print('angel_gopher example listening at ${gopher.uri}');
+}
